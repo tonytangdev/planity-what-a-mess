60220e37d31736e483aeb035764a6c8a86cb5e09
tonytangdev <tony.tang.dev@gmail.com>
2021-04-29 00:03:39 +0200

    response type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
 import { informationData } from './interfaces/informationData'
 import { jobData } from './interfaces/jobData.interface'
+import { response } from './interfaces/response.interface'
 import { userData } from './interfaces/userData.interface'
 
 const INFO_URL = "https://recrutement-practice-default-rtdb.firebaseio.com/informations.json"
@@ -10,20 +11,20 @@ const JOBS_URL = "https://recrutement-practice-default-rtdb.firebaseio.com/jobs.
 const USERS_URL = "https://recrutement-practice-default-rtdb.firebaseio.com/users.json"
 
 
-const getInformationData = async (): Promise<Map<string, informationData>> => {
+const getInformationData = async (): Promise<response<informationData>> => {
     const res = await fetch(INFO_URL)
     const json = await res.json()
 
-    return json as Map<string, informationData>
+    return json as response<informationData>
 }
 
-const getJobsData = async (): Promise<Map<string, jobData>> => {
+const getJobsData = async (): Promise<response<jobData>> => {
     const res = await fetch(JOBS_URL)
     const json = await res.json()
     return json
 }
 
-const getUsersData = async (): Promise<Map<string, userData>> => {
+const getUsersData = async (): Promise<response<userData>> => {
     const res = await fetch(USERS_URL)
     const json = await res.json()
     return json
@@ -32,12 +33,21 @@ const getUsersData = async (): Promise<Map<string, userData>> => {
 const start = async () => {
 
     const infoData = await getInformationData()
+    console.log(infoData)
 
     const jobsData = await getJobsData()
+    console.log(jobsData)
 
     const usersData = await getUsersData()
-
     console.log(usersData)
+
+    // Merge data
+    // each field has a prefix to know where it is from
+
+    // filter fields to select the correct ones (sanitize + sort)
+
+    // output is done
+
 }
 
 start()
---
+++ src/interfaces/response.interface.ts
@@ -0,0 +1,4 @@
+
+export interface response<T> {
+    [id: string]: T
+}
